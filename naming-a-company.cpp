2deb148e85da4adbf170e644974d737ae6b08365
tnilesh02 <ntsocialfilms@gmail.com>
2023-09-03 11:28:33 +0530

    beats 63%

---
+++ naming-a-company.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        unordered_map<string,int> mp;
+        int c1[26]={0};
+        for(int i=0;i<ideas.size();i++){
+            mp[ideas[i]]=1;
+            c1[ideas[i][0]-'a']++;
+        }
+        int count[26][26]={0};
+        int long long ret = 0;
+        for(int i=0;i<ideas.size();i++){
+            string x = ideas[i];
+            for(char p = 'a';p<='z';p++){
+                x[0]=p;
+                if(mp.find(x)==mp.end()){
+                    count[ideas[i][0]-'a'][p-'a']++;
+                }
+            }
+        }
+        for(int i=0;i<26;i++){
+            for(int j=0;j<26;j++){
+                ret = ret + count[i][j]*count[j][i];
+            }
+        }
+        return ret;
+    }
+};
